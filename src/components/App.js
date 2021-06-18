385ccdae192651c7f5094c52abee806fef9ca959
jonahkula <jonahkula@gmail.com>
2021-06-18 15:59:29 -0700

    moved files

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,29 @@
 import React from "react";
-import Signup from "./Signup";
-import Login from "./Login";
+import Signup from "./authentication/Signup";
+import Login from "./authentication/Login";
 import Dashboard from "./Dashboard";
-import PrivateRoute from "./PrivateRoute";
-import ForgotPassword from "./ForgotPassword";
-import UpdateProfile from "./UpdateProfile";
-import { Container } from "react-bootstrap";
+import PrivateRoute from "./authentication/PrivateRoute";
+import ForgotPassword from "./authentication/ForgotPassword";
+import UpdateProfile from "./authentication/UpdateProfile";
+import CenteredContainer from "./authentication/CenteredContainer";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
+    <CenteredContainer>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </CenteredContainer>
   );
 }
 
--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -1,12 +1,13 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 
-export default function Login() {
+export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { login } = useAuth();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
@@ -14,14 +15,21 @@ export default function Login() {
   async function handleSubmit(e) {
     e.preventDefault();
 
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      passwordRef.current.value = "";
+      passwordConfirmRef.current.value = "";
+      return setError("Passwords do not match");
+    }
+
     try {
       setError("");
       setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
+      await signup(emailRef.current.value, passwordRef.current.value);
       history.push("/");
     } catch {
-      setError("Account sign in failed");
+      setError("Account creation failed");
       passwordRef.current.value = "";
+      passwordConfirmRef.current.value = "";
       setLoading(false);
     }
   }
@@ -30,33 +38,33 @@ export default function Login() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-3">Log In</h2>
+          <h2 className="text-center mb-3">Sign Up</h2>
 
           {error && <Alert variant="danger">{error}</Alert>}
 
-          <Form className="mb-3" onSubmit={handleSubmit}>
+          <Form className="mb-2" onSubmit={handleSubmit}>
             <Form.Group id="email" className="mb-2">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id="password" className="mb-4">
+            <Form.Group id="password" className="mb-2">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
+            <Form.Group id="password-confirm" className="mb-4">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
 
             <Button disabled={loading} type="submit" className="w-100">
-              Log In
+              Sign Up
             </Button>
           </Form>
-
-          <div className="w-100 text-center mb-1">
-            <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
         </Card.Body>
       </Card>
 
       <div className="w-100 text-center mt-3">
-        Don't have an account? <Link to="/signup">Sign Up</Link>
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
     </>
   );
