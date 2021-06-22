141e992af8246e0145b2d06fea7e9a099b307679
jonahkula <jonahkula@gmail.com>
2021-06-21 17:25:12 -0700

    switched to production firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,24 +4,24 @@ import "firebase/firestore";
 import "firebase/storage";
 
 // comment out for using development version of firebase project
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FB_API_KEY_DEV,
-  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN_DEV,
-  projectId: process.env.REACT_APP_FB_PROJECT_ID_DEV,
-  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET_DEV,
-  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID_DEV,
-  appId: process.env.REACT_APP_FB_APP_ID_DEV,
-});
+// const app = firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FB_API_KEY_DEV,
+//   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN_DEV,
+//   projectId: process.env.REACT_APP_FB_PROJECT_ID_DEV,
+//   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET_DEV,
+//   messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID_DEV,
+//   appId: process.env.REACT_APP_FB_APP_ID_DEV,
+// });
 
 // comment out for using production version of firebase project
-// const app = firebase.initializeApp({
-//   apiKey: process.env.REACT_APP_FB_API_KEY_PROD,
-//   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN_PROD,
-//   projectId: process.env.REACT_APP_FB_PROJECT_ID_PROD,
-//   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET_PROD,
-//   messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID_PROD,
-//   appId: process.env.REACT_APP_FB_APP_ID_PROD,
-// });
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FB_API_KEY_PROD,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN_PROD,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID_PROD,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET_PROD,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID_PROD,
+  appId: process.env.REACT_APP_FB_APP_ID_PROD,
+});
 
 const firestore = app.firestore();
 export const database = {
